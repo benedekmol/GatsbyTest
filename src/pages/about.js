2da2beaa9507f4f4616d0b9c9f0867f9fd76c5a2
benedekmol <molnar.benedek.mate@gmail.com>
2021-08-27 21:17:21 +0200

    test ended

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ export default function About() {
     return (
         <div>
             <h1>About page test resolved ??</h1>
+            <img src="/images/hello.png"></img>
         </div>
     )
 }
