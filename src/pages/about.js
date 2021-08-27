fd28dc6bbfefcd83ac141828074ead8fafd77ebe
benedekmol <molnar.benedek.mate@gmail.com>
2021-08-27 20:35:03 +0200

    main test

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>About page test</h1>
+            <h1>About page test main</h1>
         </div>
     )
 }
